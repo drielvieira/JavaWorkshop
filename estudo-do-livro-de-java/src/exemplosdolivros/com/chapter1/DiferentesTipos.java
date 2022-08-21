b0cec650ac25ec545747d5016d84adaf9c0271d7
Adriel <Adriel@192.168.5.101>
2022-08-20 23:25:57 -0300

    commit do estudo do capitulo um

---
+++ estudo-do-livro-de-java/src/exemplosdolivros/com/chapter1/DiferentesTipos.java
@@ -0,0 +1,24 @@
+package exemplosdolivros.com.chapter1;
+
+public class DiferentesTipos {
+
+	public static void main(String[] args) {
+		// Imprimindo diferentes tipos
+		String t = "This is a text";
+		char c = 'M';
+		int n = 56;
+		float f = 56.9f;
+		double df =  1.88;
+		boolean bl = false;
+		
+		System.out.println(t);
+		System.out.println(c);
+		System.out.println(n);
+		System.out.println(f);
+		System.out.println(df);
+		System.out.println(bl);
+		
+
+	}
+
+}
---
+++ estudo-do-livro-de-java/src/exemplosdolivros/com/chapter1/Exemplo3.java
@@ -0,0 +1,20 @@
+package exemplosdolivros.com.chapter1;
+
+public class Exemplo3 {
+
+	public static void main(String[] args) {
+		
+		// seis tipos diferentes de dados
+		String t = "This is text";
+		System.out.println("This is text"); // string
+		System.out.println('B'); // char
+		System.out.println(53);  // int
+		System.out.println(23.08f); // float
+		System.out.println(1.98); // double
+		System.out.println(false); // boolean
+		
+		
+
+	}
+
+}
