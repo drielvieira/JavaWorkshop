afd48a60d0a254dc4d61445367d30ff11dff1afe
Adriel <Adriel@192.168.5.101>
2022-08-22 23:05:44 -0300

    Criando um tipo null

---
+++ estudo-do-livro-de-java/src/exemplosdolivros/com/chapter1/Exemplo04.java
@@ -0,0 +1,19 @@
+package exemplosdolivros.com.chapter1;
+
+public class Exemplo04 {
+
+	public static void main(String[] args) {
+		/* Criando uma variável vazia do 
+		 * tipo string e modificando o valor enquanto o porgrama
+		 * está em execução */
+		
+		String t = null;
+		System.out.println(t);
+		
+		/* atribuindo um novo valor a variavel vazia */
+		t = "adriel ...";
+		System.out.println(t);
+
+	}
+
+}
