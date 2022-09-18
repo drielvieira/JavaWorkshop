f40da98b4a0f8017eebfa5f460d8539bfa88370f
Adriel <Adriel@192.168.5.101>
2022-09-18 15:32:38 -0300

    Hipotenusa

--- estudo-do-livro-de-java/src/exemplosdolivros/com/chapter1/Hipotenusa.java
+++ estudo-do-livro-de-java/src/exemplosdolivros/com/chapter1/Hipotenusa.java
@@ -3,14 +3,14 @@ package exemplosdolivros.com.chapter1;
 public class Hipotenusa {
 
 	public static void main(String[] args) {
-		double a = 3; // primeiro lado do triângulo
-		double b = 4; // segundo lado do triângulo
-		double h = 0; // lado da hipotenusa com inicio 0
+		double a = 3; // PRIMEIRO LADO DO TRIÂNGULO
+		double b = 5; // SEGUNDO LADO DO TRIÂNGULO
+		double h = 0; // LADO DA HIPOTENUSA COM INICIO 0
 		
-		// Equação para resolver a hipotenusa
+		// EQUAÇÃO PARA RESOLVER A HIPOTENUSA
 		
 		h = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-		System.out.println(h);  // mostra o resultado na tela.
+		System.out.println(h);  // MOSTRA O RESULTADO NA TELA
 
 	}
 
