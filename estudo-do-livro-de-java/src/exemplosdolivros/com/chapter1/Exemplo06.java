196406e2132da8ed1f14d1640270b838664d0edd
Adriel <Adriel@192.168.5.101>
2022-09-18 15:32:02 -0300

    Vendo biblioteca Math.

--- estudo-do-livro-de-java/src/exemplosdolivros/com/chapter1/Exemplo06.java
+++ estudo-do-livro-de-java/src/exemplosdolivros/com/chapter1/Exemplo06.java
@@ -4,10 +4,18 @@ public class Exemplo06 {
 
 	public static void main(String[] args) {
 		float f = 52.48f;
-		System.out.println(f);
+		System.out.println("VALOR FLOAT: " + f);
 		
 		int i = Math.round(f);
-		System.out.println(i);
+		System.out.println("ROUND: " + i);
+		
+		System.out.println("===========================");
+		
+		float a = 49.75f;
+		System.out.println("VALOR FLOAT: " + a);
+		
+		int b = Math.round(a);
+		System.out.println("ROUND: " + b);
 	}
 
 }
